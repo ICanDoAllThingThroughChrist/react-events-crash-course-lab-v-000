5ec1e663eb4f9b1ff1dbf2e8e9de64712f6d7043
charlie  lee <leetochia@gmail.com>
2019-05-16 07:24:38 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -4,14 +4,17 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 export default class ChromeBoisDomain extends Component {
 
-  handleMouseMove = (event) => {
-    
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
+
+  /* TODO: This method should capture the `x` and `y` coordinates of the mouse
      * from the event and use them to invoke the `drawChromeBoiAtCoords`
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
-     */
-  }
+     * */
+     handleMouseMove(event) {
+         debugger
+         drawChromeBoiAtCoords(event.clientX,event.clientY)
+          debugger
+       }
 
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
@@ -24,15 +27,38 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-'
    */
+     onKeyPress ={ (event) => {
+      debugger
+      if (event.key === 'a'){
+        resize('+')
+        }
+      if (event.key === 's'){
+        resize('-')
+        }
+      }
+    }
 
-  render() {
-    return (
-      <canvas
-        onMouseMove={this.handleMouseMove}
-        width='900'
-        height='600'
-        tabIndex="0">
-      </canvas>
-    )
+<button onClick={this.tickle}>Tickle me!</button>
+    render() {
+        /*debugger*/
+      return (
+        <canvas
+          onMouseMove={this.handleMouseMove}
+          onClick={this.toggleCycling}
+          debugger
+          onKeyPress=={ (event) => {
+           if (event.key === 'a'){
+             resize('+')
+             }
+           if (event.key === 's'){
+             resize('-')
+             }
+           }
+         }
+          width='900'
+          height='600'
+          tabIndex="0">
+        </canvas>
+      )
+    }
   }
-}
